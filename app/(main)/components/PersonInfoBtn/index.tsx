019c11a709a3330101da2cf8c9f59a4d0151bb44
James Pinkney <murrowblue24@yahoo.com>
2024-03-13 16:58:58 -0400

    finished initial styling of PersonInfoBtn

--- app/(main)/components/PersonInfoBtn/index.tsx
+++ app/(main)/components/PersonInfoBtn/index.tsx
@@ -1,14 +1,65 @@
 import classes from './style.module.scss'
+import Image from 'next/image'
 
-const { infoBtnCntr } = classes
+import personIcon from '../../../../public/assets/imgs/avatars/person.jpg'
 
+const { infoBtnCntr, btnAvatarSection, btnAvatarCntr, btnAvatar } = classes
+const { btnInfoSection, btnInfoCntr, btnInfo } = classes
+
+interface IPersonInfo {
+    profilePath: string
+    name: string
+    age: string
+    city: string
+
+} 
 
 export default function PersonInfoBtn({ value }:{ value: string }) {
+
+    const tempPerson: IPersonInfo = {
+        profilePath: '../../../../public/assets/imgs/avatars/person.jpg',
+        name: 'jane doe',
+        age: '31', 
+        city: 'New York'
+    }
 
     return (
         <li className={infoBtnCntr} >
             <button>
-                <h1><span>{value}</span></h1>
+                <section className={btnAvatarSection}>
+                    <picture className={btnAvatarCntr}>
+                        {/* <Image className={btnAvatar} src={tempPerson.profilePath} alt="Person Image icon" /> */}
+                        <img className={btnAvatar} src='assets/imgs/avatars/person.jpg' alt="Person Image icon" />
+                    </picture>
+                </section>
+                <section className={btnInfoSection}>
+                    <div className={btnInfoCntr}>
+                        <div className={btnInfo}>
+                            <label>
+                                <span>Name: </span>
+                            </label>
+                            <div>
+                                <span>{tempPerson.name}</span>
+                            </div>
+                        </div>
+                        <div className={btnInfo}>
+                            <label>
+                                <span>Age: </span>
+                            </label>
+                            <div>
+                                <span>{tempPerson.age}</span>
+                            </div>
+                        </div>
+                        <div className={btnInfo}>
+                            <label>
+                                <span>City: </span>
+                            </label>
+                            <div>
+                                <span>{tempPerson.city}</span>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </button>
         </li>
     )
